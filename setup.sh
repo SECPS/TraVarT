fd29ed95399310313fd0df90e662ea7ca51e2f72
Dario Romano <AK121159@sourcery.im.jku.at>
2021-04-16 08:44:12 +0200

    Placed installation scripts
    put installation scripts and adapted paths

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+mvn install:install-file -Dfile="travart.aggregator/lib/de.ovgu.featureide.lib.fm-v3.7.0.jar" -DgroupId="de.ovgu.featureide" -DartifactId="lib.fm" -Dversion="3.7.0" -Dpackaging="jar" &
+mvn install:install-file -Dfile="travart.aggregator/lib/de.ovgu.featureide.lib.fm-3.6.2.jar" -DgroupId="de.ovgu.featureide" -DartifactId="lib.fm" -Dversion="3.6.2" -Dpackaging="jar" &
+mvn install:install-file -Dfile="travart.aggregator/lib/uvl-parser-0.1.0-SNAPSHOT-standalone.jar" -DgroupId="de.neominik" -DartifactId="uvl" -Dversion="0.1.0-SNAPSHOT" -Dpackaging="jar" &
+mvn install:install-file -Dfile="travart.aggregator/lib/ppr-dsl-20210319.jar" -DgroupId="at.sqi.ppr" -DartifactId="ppr.dsl" -Dversion="0.0.2" -Dpackaging="jar" &
+mvn install:install-file -Dfile="travart.aggregator/lib/ppr-model-20210319.jar" -DgroupId="at.sqi.ppr" -DartifactId="ppr.model" -Dversion="0.0.2" -Dpackaging="jar"
