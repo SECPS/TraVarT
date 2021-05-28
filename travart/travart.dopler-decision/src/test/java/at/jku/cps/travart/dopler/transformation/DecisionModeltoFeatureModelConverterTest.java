ab44370b15caeb3e8c144e7cdde8f030bc20d229
Dario Romano <AK121159@sourcery.im.jku.at>
2021-05-28 11:46:09 +0200

    Update DecisionModeltoFeatureModelConverterTest.java
    remove unused import

--- travart/travart.dopler-decision/src/test/java/at/jku/cps/travart/dopler/transformation/DecisionModeltoFeatureModelConverterTest.java
+++ travart/travart.dopler-decision/src/test/java/at/jku/cps/travart/dopler/transformation/DecisionModeltoFeatureModelConverterTest.java
@@ -40,7 +40,6 @@ import de.ovgu.featureide.fm.core.base.FeatureUtils;
 import de.ovgu.featureide.fm.core.base.IConstraint;
 import de.ovgu.featureide.fm.core.base.IFeature;
 import de.ovgu.featureide.fm.core.base.IFeatureModel;
-import de.ovgu.featureide.fm.core.base.impl.Constraint;
 import de.ovgu.featureide.fm.core.base.impl.DefaultFeatureModelFactory;
 import de.ovgu.featureide.fm.core.base.impl.Feature;
 import de.ovgu.featureide.fm.core.base.impl.FeatureModel;
