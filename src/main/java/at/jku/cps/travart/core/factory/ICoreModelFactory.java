8b90d122bbda38dc1cbdac1ef685c877dfe01d41
Kevin Feichtinger <kevin.feichtinger@jku.at>
2022-12-20 17:53:57 +0100

    added factory to create core model (UVL) elements

--- src/main/java/at/jku/cps/travart/core/factory/ICoreModelFactory.java
+++ src/main/java/at/jku/cps/travart/core/factory/ICoreModelFactory.java
@@ -1,8 +1,32 @@
 package at.jku.cps.travart.core.factory;
 
 import at.jku.cps.travart.core.common.IFactory;
+import de.vill.model.Feature;
 import de.vill.model.FeatureModel;
+import de.vill.model.constraint.AndConstraint;
+import de.vill.model.constraint.Constraint;
+import de.vill.model.constraint.EquivalenceConstraint;
+import de.vill.model.constraint.ImplicationConstraint;
+import de.vill.model.constraint.LiteralConstraint;
+import de.vill.model.constraint.NotConstraint;
+import de.vill.model.constraint.OrConstraint;
+import de.vill.model.constraint.ParenthesisConstraint;
 
 public interface ICoreModelFactory extends IFactory<FeatureModel> {
 
+	Feature createFeature(String id);
+
+	ImplicationConstraint createImplicationConstraint(Constraint left, Constraint right);
+
+	EquivalenceConstraint createEquivalenceConstraint(Constraint left, Constraint right);
+
+	AndConstraint createAndConstraint(Constraint left, Constraint right);
+
+	OrConstraint createOrConstraint(Constraint left, Constraint right);
+
+	NotConstraint createNotConstraint(Constraint constraint);
+
+	ParenthesisConstraint createParenthesisConstraint(Constraint constraint);
+
+	LiteralConstraint createLiteralConstraint(String id);
 }
--- src/main/java/at/jku/cps/travart/core/factory/impl/CoreModelFactory.java
+++ src/main/java/at/jku/cps/travart/core/factory/impl/CoreModelFactory.java
@@ -1,7 +1,18 @@
 package at.jku.cps.travart.core.factory.impl;
 
+import java.util.Objects;
+
 import at.jku.cps.travart.core.factory.ICoreModelFactory;
+import de.vill.model.Feature;
 import de.vill.model.FeatureModel;
+import de.vill.model.constraint.AndConstraint;
+import de.vill.model.constraint.Constraint;
+import de.vill.model.constraint.EquivalenceConstraint;
+import de.vill.model.constraint.ImplicationConstraint;
+import de.vill.model.constraint.LiteralConstraint;
+import de.vill.model.constraint.NotConstraint;
+import de.vill.model.constraint.OrConstraint;
+import de.vill.model.constraint.ParenthesisConstraint;
 
 public final class CoreModelFactory implements ICoreModelFactory {
 
@@ -33,6 +44,46 @@ public final class CoreModelFactory implements ICoreModelFactory {
 	@Override
 	public FeatureModel create() {
 		return new FeatureModel();
+	}
+
+	@Override
+	public Feature createFeature(final String id) {
+		return new Feature(Objects.requireNonNull(id));
+	}
+
+	@Override
+	public ImplicationConstraint createImplicationConstraint(final Constraint left, final Constraint right) {
+		return new ImplicationConstraint(Objects.requireNonNull(left), Objects.requireNonNull(right));
+	}
+
+	@Override
+	public EquivalenceConstraint createEquivalenceConstraint(final Constraint left, final Constraint right) {
+		return new EquivalenceConstraint(Objects.requireNonNull(left), Objects.requireNonNull(right));
+	}
+
+	@Override
+	public AndConstraint createAndConstraint(final Constraint left, final Constraint right) {
+		return new AndConstraint(Objects.requireNonNull(left), Objects.requireNonNull(right));
+	}
+
+	@Override
+	public OrConstraint createOrConstraint(final Constraint left, final Constraint right) {
+		return new OrConstraint(Objects.requireNonNull(left), Objects.requireNonNull(right));
+	}
+
+	@Override
+	public NotConstraint createNotConstraint(final Constraint constraint) {
+		return new NotConstraint(Objects.requireNonNull(constraint));
+	}
+
+	@Override
+	public ParenthesisConstraint createParenthesisConstraint(final Constraint constraint) {
+		return new ParenthesisConstraint(Objects.requireNonNull(constraint));
+	}
+
+	@Override
+	public LiteralConstraint createLiteralConstraint(final String id) {
+		return new LiteralConstraint(Objects.requireNonNull(id));
 	}
 
 }
