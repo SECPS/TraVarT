ff6af01becef833f92ea2e1d9eae6472209385cf
Kevin Feichtinger <kevin.feichtinger@jku.at>
2023-04-27 10:14:02 +0200

    added copyright header

--- src/main/java/at/jku/cps/travart/core/helpers/TraVarTUtils.java
+++ src/main/java/at/jku/cps/travart/core/helpers/TraVarTUtils.java
@@ -1,3 +1,12 @@
+/*******************************************************************************
+ * TODO: explanation what the class does
+ *  
+ * @author Kevin Feichtinger
+ *  
+ * Copyright 2023 Johannes Kepler University Linz
+ * LIT Cyber-Physical Systems Lab
+ * All rights reserved
+ *******************************************************************************/
 package at.jku.cps.travart.core.helpers;
 
 import static at.jku.cps.travart.core.transformation.DefaultModelTransformationProperties.ABSTRACT_ATTRIBUTE;
--- src/main/java/at/jku/cps/travart/core/io/UVLWriter.java
+++ src/main/java/at/jku/cps/travart/core/io/UVLWriter.java
@@ -1,3 +1,12 @@
+/*******************************************************************************
+ * TODO: explanation what the class does
+ *  
+ * @author Kevin Feichtinger
+ *  
+ * Copyright 2023 Johannes Kepler University Linz
+ * LIT Cyber-Physical Systems Lab
+ * All rights reserved
+ *******************************************************************************/
 package at.jku.cps.travart.core.io;
 
 import java.io.File;
