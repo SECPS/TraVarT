c4a16986b221a5735059183e08d566751e56831d
Kaan B. Y. <6b6279+noreply@mailbox.org>
2024-07-31 06:59:42 +0200

    Refactor IConfigurable

--- src/main/java/at/jku/cps/travart/core/common/IConfigurable.java
+++ src/main/java/at/jku/cps/travart/core/common/IConfigurable.java
@@ -18,25 +18,41 @@ package at.jku.cps.travart.core.common;
 
 /**
  * This represents a configurable item of an variablilty model. An item
- * implementing this interface can be selected.
+ * implementing this interface can be configured.
  *
  * @author Johann Stoebich
  * @author Kevin Feichtinger
  */
 public interface IConfigurable {
 	/**
-	 * Returns whether the feature is selected or not.
+	 * Returns whether this object is configured.
+	 * 
+	 * This usually refers to the existence of some concrete value configuration.
+	 * For the example of integer features (in the scope of FOP), the setting of any
+	 * value is a configuration. The lack of a configuration occurs when the value
+	 * field of the respective feature is not initialised, e.g., set to `null`.
 	 *
-	 * @return whether the feature is selected or not.
+	 * @return whether the object is configured or not
 	 */
-	boolean isSelected();
+	boolean isConfigured();
 
 	/**
-	 * De-/Selects the feature.
+	 * Configures this object.
+	 * 
+	 * What exactly "configure" means in this context, depends on the class
+	 * implementing IConfigurable. For feature-like value objects, to "configure" an
+	 * unconfigured object might just be setting the value field to a default
+	 * reference value, e.g., 0. If the object is already configured, "configuring"
+	 * the object again should not reset the configuration, but instead do nothing.
+	 * For the re-configuration, use the getter/setter of the configuration-specific
+	 * field, in case of a value object, the value field.
+	 * 
+	 * Unconfiguring a configured object should remove the configuration-specific
+	 * values from the respective fields, usually by setting this fields to `null`.
 	 *
-	 * @param selected De-/Selects the configurable.
+	 * @param selected Toggle "configuredless" of this object
 	 */
-	void setSelected(boolean selected);
+	void setConfigured(boolean selected);
 
 	/**
 	 * Returns the name of an IConfigurable of a variability model
